b191edde2de45e7bb363dbc465c0bfb58dd46dd8
Shubham Jain <shubham.jain4@MacBook-Pro-2.local>
2023-03-06 15:59:56 +0530

    alive nieghbours

--- gameOfLife.js
+++ gameOfLife.js
@@ -16,6 +16,14 @@ function main() {
 
 function startGameOfLife(rows, columns, ticks) {
   initialiseGrid(rows, columns);
+  console.log("Initial Grid");
+  displayGrid(grid);
+  for (let index = 0; index < ticks; index++) {
+    var tickNumber = index + 1;
+    console.log("Tick number: ", tickNumber);
+    manipulateGrid(grid);
+    displayGrid(grid);
+  }
 }
 
 function initialiseGrid(rows, columns) {
@@ -27,11 +35,67 @@ function initialiseGrid(rows, columns) {
     }
     grid.push(row);
   }
-  displayGrid(grid);
 }
 
 function displayGrid(grid) {
   console.log(grid);
+}
+
+function manipulateGrid(grid) {
+  for (let rowIndex = 0; rowIndex < grid.length; rowIndex++) {
+    var row = grid[rowIndex];
+    for (let columnIndex = 0; columnIndex < row.length; columnIndex++) {
+      var cell = row[columnIndex];
+      if (isLive(cell)) {
+        console.log("Cell is live");
+        var aliveNeighbours = getAliveNeighbours(grid, rowIndex, columnIndex);
+        console.log("Alive neighbours: ", aliveNeighbours);
+      } else {
+        console.log("Cell is dead");
+      }
+    }
+  }
+}
+
+function isLive(cell) {
+  return cell == 1;
+}
+
+function getAliveNeighbours(grid, rowIndex, columnIndex) {
+  var alive = 0;
+  var neighbours = [];
+
+  if (rowIndex - 1 >= 0) {
+    if (columnIndex + 1 < grid[0].length) {
+      neighbours.push(grid[rowIndex - 1][columnIndex + 1]);
+    }
+    if (columnIndex - 1 >= 0) {
+      neighbours.push(grid[rowIndex - 1][columnIndex - 1]);
+    }
+    neighbours.push(grid[rowIndex - 1][columnIndex]);
+  }
+  if (rowIndex + 1 < grid.length) {
+    if (columnIndex + 1 < grid[0].length) {
+      neighbours.push(grid[rowIndex + 1][columnIndex + 1]);
+    }
+    if (columnIndex - 1 >= 0) {
+      neighbours.push(grid[rowIndex + 1][columnIndex - 1]);
+    }
+    neighbours.push(grid[rowIndex + 1][columnIndex]);
+  }
+  if (columnIndex + 1 < grid[0].length) {
+    neighbours.push(grid[rowIndex][columnIndex + 1]);
+  }
+  if (columnIndex - 1 >= 0) {
+    neighbours.push(grid[rowIndex][columnIndex - 1]);
+  }
+
+  neighbours.forEach((cell) => {
+    if (cell == 1) {
+      alive += 1;
+    }
+  });
+  return alive;
 }
 
 main();
