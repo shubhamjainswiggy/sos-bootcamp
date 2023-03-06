9619bf5ba4e4c9d68d7a81ce12beb73774a9b215
Shubham Jain <shubham.jain4@MacBook-Pro-2.local>
2023-03-06 15:01:35 +0530

    user input

---
+++ gameOfLife.js
@@ -0,0 +1,14 @@
+function main() {
+    console.log("Hello")
+    var m = prompt("Enter number of rows")
+    var n = prompt("Enter number of columns")
+    var t = prompt("Enter number of ticks")
+    var rows = parseInt(m)
+    var columns = parseInt(n)
+    var ticks = parseInt(t)
+    console.log("rows: ", rows)
+    console.log("columns: ", columns)
+    console.log("ticks: ", ticks)
+}
+
+main()
