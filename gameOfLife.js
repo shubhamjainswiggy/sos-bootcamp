d7a27c659cc46b51b8564627311279ab551eb38a
Shubham Jain <shubham.jain4@MacBook-Pro-2.local>
2023-03-06 15:12:11 +0530

    initialise grid

--- gameOfLife.js
+++ gameOfLife.js
@@ -1,14 +1,33 @@
+var grid = [];
+
 function main() {
-    console.log("Hello")
-    var m = prompt("Enter number of rows")
-    var n = prompt("Enter number of columns")
-    var t = prompt("Enter number of ticks")
-    var rows = parseInt(m)
-    var columns = parseInt(n)
-    var ticks = parseInt(t)
-    console.log("rows: ", rows)
-    console.log("columns: ", columns)
-    console.log("ticks: ", ticks)
+  console.log("Hello");
+  var m = prompt("Enter number of rows");
+  var n = prompt("Enter number of columns");
+  var t = prompt("Enter number of ticks");
+  var rows = parseInt(m);
+  var columns = parseInt(n);
+  var ticks = parseInt(t);
+  console.log("rows: ", rows);
+  console.log("columns: ", columns);
+  console.log("ticks: ", ticks);
+  startGameOfLife(rows, columns, ticks);
 }
 
-main()
+function startGameOfLife(rows, columns, ticks) {
+  initialiseGrid(rows, columns);
+}
+
+function initialiseGrid(rows, columns) {
+  for (let index = 0; index < rows; index++) {
+    var row = [];
+    for (let index = 0; index < columns; index++) {
+      var cell = Math.round(Math.random());
+      row.push(cell);
+    }
+    grid.push(row);
+  }
+  console.log(grid);
+}
+
+main();
